da218229b62bc8f30d381ece388d7b802e089943
Hess Wang <hess90wang@gmail.com>
2023-11-29 15:48:36 +0900

    init

---
+++ src/components/CreateTodo.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const TodoForm = styled.form``;
+
+const TodoInput = styled.input``;
+
+const TodoBtn = styled.button``;
+
+export default function CreateTodo() {
+  return (
+    <>
+      <TodoForm>
+        <TodoInput />
+        <TodoBtn />
+      </TodoForm>
+    </>
+  );
+}
